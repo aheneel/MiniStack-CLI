1ff4394f9ea28d0c5c486e60859eab880bd22e7e
aheneel <aheneel@gmail.com>
2025-06-28 23:29:36 +0300

    Update show_info.sh

--- show_info.sh
+++ show_info.sh
@@ -3,6 +3,10 @@
 
 show_info() {
     clean_old_logs
+    if [ $# -gt 0 ]; then
+        log_message "error" "Неверные аргументы для --info: $@. Используйте без флагов"
+        exit 1
+    fi
     log_message "info" "Проверяем статус сервисов..." "start_operation"
     systemctl status nginx --no-pager
     for version in "${PHP_VERSIONS[@]}"; do
