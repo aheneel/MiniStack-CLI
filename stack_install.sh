9b71a85f34f152762a1b65c8dbbf5219c0a415eb
aheneel <aheneel@gmail.com>
2025-06-29 00:17:47 +0300

    Update stack_install.sh

--- stack_install.sh
+++ stack_install.sh
@@ -32,9 +32,11 @@ install_stack() {
     openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/CN=localhost" || { log_message "error" "Не удалось создать самоподписанный сертификат"; exit 1; }
     chmod 600 /etc/ssl/private/nginx-selfsigned.key
     log_message "success" "Самоподписанный SSL-сертификат создан!"
-    mkdir -p /var/www/html
-    chown www-data:www-data /var/www/html
-    chmod 755 /var/www/html
+    if [ -d "/var/www/html" ]; then
+        rm -rf /var/www/html/*
+    else
+        mkdir -p /var/www/html
+    fi
     echo "MiniStack CLI" > /var/www/html/index.html
     chmod 644 /var/www/html/index.html
     log_message "success" "Дефолтный index.html создан!"
@@ -83,9 +85,9 @@ EOL
             sed -i 's/max_execution_time = .*/max_execution_time = 300/' "$PHP_INI"
             sed -i 's/max_input_time = .*/max_input_time = 300/' "$PHP_INI"
             sed -i 's/expose_php = On/expose_php = Off/' "$PHP_INI"
-            log_message "success" "Конфигурация PHP $version обновлена"
+            log_message "success" "PHP $version: php.ini настроен"
         else
-            log_message "error" "Файл php.ini для PHP $version не найден"
+            log_message "error" "Файл $PHP_INI не найден"
             exit 1
         fi
         systemctl enable php${version}-fpm
